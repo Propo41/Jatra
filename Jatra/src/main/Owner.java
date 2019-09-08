3db5ffed6579802fe0858d10f834939c95d2d1cb
Ali Ahnaf <aliahnaf327@gmail.com>
2019-09-08 15:25:07 +0600

    added clean files

---
+++ Jatra/src/main/Owner.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.List;
+
+/**
+ *
+ * @author Swapnil
+ */
+public class Owner extends User {
+
+    private List<Bus> busList;
+
+    public Owner() {
+
+    }
+
+    // the busList object will be created from the signup page
+    public Owner(String username, String phoneNumber, String city, String password, String email, List<Bus> busList) {
+        super(username, phoneNumber, city, password, email);
+        setServiceType(OWNER);
+        this.busList = busList;
+    }
+
+    public Owner(String username, String phoneNumber, String city, String password, String email) {
+        super(username, phoneNumber, city, password, email);
+        setServiceType(OWNER);
+        this.busList = busList;
+    }
+
+    public List<Bus> getBusList() {
+        return busList;
+    }
+
+    public void setBusList(List<Bus> busList) {
+        this.busList = busList;
+    }
+
+}
